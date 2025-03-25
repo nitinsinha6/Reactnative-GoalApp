53cb1377d9aa1642c7747a2d4709b6ffe373512f
nitinsinha6 <nitinsinha6@gmail.com>
2025-03-25 14:42:12 -0700

    Delete Item Function

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,10 +1,13 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Pressable} from 'react-native';
 function GoalItem(props) {
 
     return (
-        <View style={styles.goalItem}>
+      <Pressable onPress={props.onDeleteItem.bind(this,props.id)}> 
+         <View style={styles.goalItem}>
         <Text style={styles.goalText}>{props.text}</Text>
       </View>
+      </Pressable>
+       
     )
 }
 
